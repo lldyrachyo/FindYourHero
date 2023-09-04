1b96d2c5d8bb5c7595aa2cabdab5e548d34f18e9
Mark <papushman@yandex.ru>
2023-09-04 13:45:12 +0300

    NetworkManager created

---
+++ FindYourHero/NetworkManager.swift
@@ -0,0 +1,56 @@
+//
+//  NetworkManager.swift
+//  FindYourHero
+//
+//  Created by Mark Papush on 9/4/23.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+}
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchData(completion: @escaping(Result<[Superhero], NetworkError>) -> Void) {
+        guard let url = URL(string: "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json") else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                completion(.failure(.noData))
+                return
+            }
+            do {
+                let superheroes = try JSONDecoder().decode([Superhero].self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(superheroes))
+                }
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        }.resume()
+    }
+    
+    func fetchImage(from url: URL, completion: @escaping(Result<Data, NetworkError>) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                completion(.failure(.noData))
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            DispatchQueue.main.async {
+                completion(.success(data))
+            }
+            
+        }.resume()
+    }
+}
